d3ce6843106369d5a5d79c46816f7b5c83689476
JoelJovin <82594202+JoelJovin@users.noreply.github.com>
2023-06-23 10:24:21 +0530

    My First MVN project

---
+++ src/main/java/com/fssa/first/sample/HelloWorldProgs.java
@@ -0,0 +1,10 @@
+package com.fssa.first.sample;
+
+public class HelloWorldProgs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
